a307742ba4f1d9b57ad866746c330148fb0f9375
khoitq-acl <khoitq@americancodelab.com>
2024-09-08 01:21:30 +0700

    feat: add tags

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,9 @@ from langchain_community.llms import Ollama
 app = FastAPI()
 
 # app.include_router(router=room.router, prefix="/room")
-app.include_router(router=chat.router, prefix="/chat")
+app.include_router(router=chat.router, prefix="/chat", tags=['Chat'])
 
 
 @app.get("/")
-async def read_root():
+async def hello():
     return {"message": "Welcome to the Room API"}
